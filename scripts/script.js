6a831ce58e3221c16cc062de3ee549da58878c0c
SousaaArthur <arthursousaa2004@gmail.com>
2024-04-08 16:37:16 -0300

    bookflix v1.0

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+const pdfUrl = './books/eBooks/Sumario.pdf';
+const pdfContainer = document.getElementById('pdf-container');
+
+pdfjsLib.getDocument(pdfUrl).promise.then(pdf => {
+    for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        pdf.getPage(pageNum).then(page => {
+            const viewport = page.getViewport({ scale: 1.2 });
+            const canvas = document.createElement('canvas');
+            const context = canvas.getContext('2d');
+
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+
+            page.render({
+                canvasContext: context,
+                viewport: viewport
+            }).promise.then(() => {
+                pdfContainer.appendChild(canvas);
+            });
+        });
+    }
+});
